39d7f66d2cb5079d2fb901a7a5bea39cb7cfceb0
divcon <sungjinpark.08@gmail.com>
2014-09-21 23:48:42 +0900

    9.21

--- forez/utils/tests.py
+++ forez/utils/tests.py
@@ -1,11 +1,11 @@
 from request_form import api_request
 
 test = api_request()
-url = "/users"
-body = {'username': 'httptest', 'password': '123', 'email': 'test@naver.com'}
+url = "/tokens"
+body = {'username': 'sungjin', 'password': 'rkems'}
 
 # resp, content = test.http_request(url=url, method="POST", body=body)
 
-resp, content = test.http_request(url=url)
+resp, content = test.http_request(method="POST", url=url, body=body)
 print resp
 print content
